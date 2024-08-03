13b41553fbc50b9614619c0a7cfc522344b0ebd6
Harris Schrick <schrickh+github@gmail.com>
2024-08-02 19:49:21 -0700

    restarting

--- two-sum.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        seen = {} #key = num, val = indicie
-
-        for i in range(len(nums)):
-            if target - nums[i] in seen:
-                return [i, seen[target - nums[i]]]
-            seen[nums[i]] = i
